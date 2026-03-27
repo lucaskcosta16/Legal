d9043c527087dc76192ad349856aa663b5fee24f
Lucas <lucas.k.costa13@gmail.com>
2026-03-27 19:32:59 -0300

    Update Projeto.py

--- Projeto.py
+++ Projeto.py
@@ -8,6 +8,10 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f'Fizemos login como {bot.user}')
+    
 resp = 0
 tent = 0
 
@@ -16,27 +20,30 @@ async def guess(ctx):
     resp = random.randint(1,100)
     tent = 5
     await ctx.send("Adivinhe um número entre 1 e 100. Você tem 5 tentativas!")
-
+    
     def check(m):
         return m.author == ctx.author and m.content.isdigit()
 
     for i in range(tent):
-        msg = bot.wait_for("message", check=check)
+        msg = await bot.wait_for("message", check=check)
 
         chute = int(msg.content)
 
         if chute == resp:
-            ctx.send(f"Você Acertou!!!")
+            await ctx.send(f"Você Acertou!!!")
+            return
         elif chute <= resp:
-            ctx.send(f'O número é MAIOR! (Tentativas restantes: {tent - i - 1})')
+            await ctx.send(f'O número é MAIOR! (Tentativas restantes: {tent - i - 1})')
         else:
-            ctx.send(f'O número é MENOR! (Tentativas restantes: {tent - i - 1})')
+            await ctx.send(f'O número é MENOR! (Tentativas restantes: {tent - i - 1})')
 
-    ctx.send(f'Suas tentativas acabaram! O número era {resp}.')
+    await ctx.send(f'Suas tentativas acabaram! O número era {resp}.')
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        bot.process_commands(message)
+        return
+    
+    await bot.process_commands(message)
 
-bot.run("O discord troca o meu token se eu botar ele aqui ent bota o seu quando for testar)
+bot.run("O discord troca o meu token se eu botar ele aqui ent bota o seu quando for testar")
