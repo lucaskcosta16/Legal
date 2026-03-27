0cefd73babae0ae435f170c666b93222cf863497
Lucas <lucas.k.costa13@gmail.com>
2026-03-27 19:08:48 -0300

    Update Projeto.py

--- Projeto.py
+++ Projeto.py
@@ -1,65 +1,42 @@
-# AVISO: ESSE CÓDIGO NÃO ESTÁ PRONTO E ESTÁ MUITO BUGADO. TALVEZ ELE NEM FUNCIONE PARA VOCÊ.
-# VOCÊ FOI AVISADO...
-
 import discord
 import random
 import asyncio
 from discord.ext import commands 
 
-# A variável intents armazena as permissões do bot
 intents = discord.Intents.default()
-# Ativar a permissão para ler o conteúdo das mensagens
 intents.message_content = True
-# Criar um bot e passar as permissões
+
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+resp = 0
+tent = 0
+
+@bot.command()
+async def guess(ctx):
+    resp = random.randint(1,100)
+    tent = 5
+    await ctx.send("Adivinhe um número entre 1 e 100. Você tem 5 tentativas!")
+
+    def check(m):
+        return m.author == ctx.author and m.content.isdigit()
+
+    for i in range(tent):
+        msg = bot.wait_for("message", check=check)
+
+        chute = int(msg.content)
+
+        if chute == resp:
+            ctx.send(f"Você Acertou!!!")
+        elif chute <= resp:
+            ctx.send(f'O número é MAIOR! (Tentativas restantes: {tent - i - 1})')
+        else:
+            ctx.send(f'O número é MENOR! (Tentativas restantes: {tent - i - 1})')
+
+    ctx.send(f'Suas tentativas acabaram! O número era {resp}.')
+
 @bot.event
-async def on_ready():
-    print(f'Fizemos login como {bot.user}')
+async def on_message(message):
+    if message.author == bot.user:
+        bot.process_commands(message)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Ola eu sou o bot {bot.user}!')
-
-@bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send(f'{member} joined on {member.joined_at}')
-
-# This example requires the 'message_content' privileged intent to function.
-
-client MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
-
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send(f'Oops. It is actually {answer}.')
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run('token')
-
-bot.run("MTQ4MjE1MTA2NzI2ODQxOTc5Ng.G27FxG.xBbLKm84T6G5kxdaTPSSt13huIKkDu0E_hcVyw")
+bot.run("O discord troca o meu token se eu botar ele aqui ent bota o seu quando for testar)
